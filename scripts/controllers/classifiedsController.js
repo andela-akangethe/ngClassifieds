8cc4d6378022ff6c5370b8175d5366c0b9b62d6e
andela-akangethe <alex.kangethe@andela.com>
2016-05-12 19:59:52 +0300

    Add ADD

--- scripts/controllers/classifiedsController.js
+++ scripts/controllers/classifiedsController.js
@@ -3,11 +3,31 @@
 
   angular
     .module('ngClassifieds')
-    .controller('classifiedsController', function($http, classifiedsFactory) {
+    .controller('classifiedsController', function($http, classifiedsFactory, $mdSidenav, $scope, $mdToast) {
       var vm = this;
 
       classifiedsFactory.getClassifieds().then(function(classifieds) {
         vm.classifieds = classifieds.data;
       });
+
+      vm.openSidebar = function() {
+        $mdSidenav('left').open();
+      }
+
+      vm.closeSidebar = function() {
+        $mdSidenav('left').close();
+      }
+
+      vm.saveClassified = function(product) {
+        if (product) {
+          vm.classifieds.push(product);
+          $scope.product = {};
+        }
+      }
+
+      vm.deleteClassifieds = function(product) {
+        var index = vm.classifieds.indexOf(product);
+        vm.classifieds.splice(index, 1);
+      }
     });
 })();
