6448cecdae0cf58f06541fe6d427ecc11173b4e1
andela-akangethe <alex.kangethe@andela.com>
2016-05-12 11:16:41 +0300

    edit index.html and app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,10 @@
 angular
-  .module('ngClassifieds', []);
+  .module('ngClassifieds', ['ngMaterial'])
+  .config(config);
+
+function config($mdThemingProvider) {
+  $mdThemingProvider
+    .theme('default')
+    .primaryPalette('teal')
+    .accentPalette('orange');
+}
